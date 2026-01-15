8cb2ba64e0a90457437cabc3a7ea656dba110976
HarshMethwani <harshmethwani2@gmail.com>
2026-01-15 22:50:12 +0530

    added streaming

--- main.py
+++ main.py
@@ -5,14 +5,31 @@ from pydantic import BaseModel, HttpUrl
 from services.filter_files import collect_files
 from services.embedding_service import search, embed_and_store,get_collection_name
 from services.chunk_service import extract_chunks_from_file
-from services.llm_service import build_context,call_llm
+from services.llm_service import build_context,call_llm,call_llm_stream
 from pathlib import Path
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
 from typing import List
 app = FastAPI()
 
 logging.basicConfig(filename='logger.log',format='%(asctime)s %(message)s',filemode='w')
 logger = logging.getLogger()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class CloneRepository(BaseModel):
     repo_url:HttpUrl
@@ -67,4 +84,13 @@ def chat(request:ChatRequest):
     sources = [{"file": str(c["metadata"]["file_path"]).replace("./data/repos","")} for c in relevant_chunks]
     answer = call_llm(request.question,request.history,context)
     return ChatResponse(answer=answer, sources=sources)
+
+@app.post('/chat/stream')
+def chat_stream(request:ChatRequest):
+    repo_name = get_collection_name(request.repo_path)
+    relevant_chunks = search(request.question,repo_name)
+    context = build_context(relevant_chunks)
+    sources = [{"file": str(c["metadata"]["file_path"]).replace("./data/repos","")} for c in relevant_chunks]
+    # answer = call_llm(request.question,request.history,context)
+    return StreamingResponse(call_llm_stream(request.question,request.history,context,sources),media_type='text/event-stream')
 
--- services/llm_service.py
+++ services/llm_service.py
@@ -3,6 +3,7 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 import os
+import json
 load_dotenv()
 
 GEMINI_KEY = os.getenv("GEMINI_API")
@@ -31,3 +32,25 @@ def call_llm(question: str,history:List[dict], context: str) -> str:
           config=types.GenerateContentConfig( system_instruction="You are a helpful code assistant. Consider the conversation history when responding. Use the provided code context to answer code questions.")
         )
       return response.text
+
+
+def call_llm_stream(question: str,history:List[dict], context: str,sources:List[dict]) -> str:
+      client = genai.Client(api_key=GEMINI_KEY)
+      messages = []
+      if history:
+            for msg in history:
+                  messages.append({
+                        "role":msg['role'],
+                        "parts":[{"text":msg["content"]}]
+                  })
+      
+      messages.append({"role":"user","parts":[{"text":f"Context:{context} Question:{question}"}]})
+      response = client.models.generate_content_stream(
+        model="gemini-2.5-flash-lite", contents=messages,
+          config=types.GenerateContentConfig( system_instruction="You are a helpful code assistant. Consider the conversation history when responding. Use the provided code context to answer code questions.")
+        )
+      yield f"data: {json.dumps({'type':'sources','data':sources})}\n\n"
+      for chunk in response:
+            yield f"data: {json.dumps({'type':'chunk','text':chunk.text})}\n\n"
+     
+      yield f"data: {json.dumps({'type':'done'})}\n\n"
